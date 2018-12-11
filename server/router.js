11692b92e65ec6cfad827ed3ef417534504b2b6b
glact1973 <44960587+glact1973@users.noreply.github.com>
2018-12-11 11:55:00 +0900

    ツッコミメッセージを表示

--- server/router.js
+++ server/router.js
@@ -1,18 +1,27 @@
 const express = require('express');
+// 追加ここから
+const getTukkomi = require('./get-random-tukkomi');
+// /追加ここまで
 
 const tukkomiRoboRouter = express.Router();
 
 tukkomiRoboRouter.get('/', (req, res) => {
   const { phrase } = req.query;
-  console.log('phrase = ', phrase);
-
   //
   // TODO
   //
   // if phrase が空っぽだったらエラーメッセージを送ろう。
-  // else phrase にツッコミを追加して送ろう。 (HINT: get-random-tukkomi.js が役に立つかも）
 
-  res.send('ツッコミタイム');
+  // 追加ここから
+  // ランダムメッセージを取得する
+  const inputPhrase = { phrase }.phrase;
+  const tukkomiMessage = getTukkomi();
+  if (inputPhrase === '')  res.send('入力してな'); 
+  // else phrase にツッコミを追加して送ろう。 (HINT: get-random-tukkomi.js が役に立つかも）
+  else { 
+    res.send(inputPhrase + '...って、' + tukkomiMessage);
+  }
+  // /追加ここまで
 });
 
 module.exports = tukkomiRoboRouter;
